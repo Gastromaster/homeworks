1d73e43d719db11e883b28503c07548b6a319034
Gastromaster <165019499+Gastromaster@users.noreply.github.com>
2024-03-30 21:05:18 +0300

    homework4.py

--- homework4.py
+++ homework4.py
@@ -2,4 +2,5 @@ immutable_var = 1, 2, True, 'string'
 print("immmutable_var: ", immutable_var)
 # immutable_var[0] = 4 потомучто кортеж нужен когда нам надо сохранить исходные данные
 mutable_list = [1, 2, True, 'string']
+mutable_list[0] = 5
 print('mutable_list: ', mutable_list)
