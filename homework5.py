514e5369fb1fde509b8766a83eda11030c416b47
Gastromaster <165019499+Gastromaster@users.noreply.github.com>
2024-03-30 22:07:21 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'orange', 'pineapple', 'banana', 'avokado']
+print('List: ', my_list)
+print('First element: ', my_list[0])
+print('Last element: ',  my_list[-1])
+print('Sublist: ', my_list[2:5])
+my_list[2] = 'apricot'
+print('Modified: ', my_list)
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'banana': 'банан'}
+print('Dictionary: ', my_dict)
+print('Translation: ',my_dict['apple'])
+my_dict['apple'] = 'не яблоко'
+print('Modified dictionary: ', my_dict)
